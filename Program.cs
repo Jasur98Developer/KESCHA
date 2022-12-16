e862f39d02c7ae6559b84c3b58645eeaebb0d04a
Jasur98Developer <jasurbekabdimuminov98@gmail.com>
2022-12-16 16:40:34 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Enter your name:  ");
+string name = Console.ReadLine();
+string greeting=$"Hello {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.WriteLine("Enter you age: ");
+string ageAsString=Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age=Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int keschasAge=3;
+int ageDifference=age -keschasAge;
+System.Console.WriteLine($"The difference betwen your and kescha'a age is {ageDifference}");
